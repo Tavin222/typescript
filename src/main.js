1a69c4a2eb400981d1306aeed3881f7a623dc0cd
tavin <otaviohero@gmail.com>
2024-07-10 20:34:21 -0300

    inicial

---
+++ src/main.js
@@ -0,0 +1,12 @@
+"use strict";
+// Função de multiplicação
+function multiplicacao(num1, num2) {
+    return num1 * num2;
+}
+// Função de saudação
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+// Exemplos de uso das funções
+console.log(multiplicacao(5, 3)); // Saída: 15
+console.log(saudacao("João")); // Saída: Olá João
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+// Função de multiplicação
+function multiplicacao(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+// Função de saudação
+function saudacao(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+// Exemplos de uso das funções
+console.log(multiplicacao(5, 3)); // Saída: 15
+console.log(saudacao("João")); // Saída: Olá João
